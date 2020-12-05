d3763c65047123d82f19b1a7bfca01ee1db04568
Sanjeev M <sanjeev.malagi@scripbox.com>
2020-12-05 14:03:56 +0530

    Add http server and a dev server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { render } from "react-dom";
 
 function App() {
-    const [state, setState] = useState("CLICK ME");
+    const [state, setState] = useState("CLICKs ME");
 
     return <button onClick={() => setState("CLICKED")}>{state}</button>;
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require("path");
 
 module.exports = {
@@ -17,8 +18,14 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', "src", "index.html")
     })
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 9000
+  }
 };
