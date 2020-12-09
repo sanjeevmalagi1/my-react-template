35217fc197b0c989f576c2f9ae2aab9a23384859
Sanjeev M <sanjeev.malagi@scripbox.com>
2020-12-09 09:24:20 +0530

    Add support for tailwind css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import './index.css';
+
 import React, { useState } from "react";
 import { render } from "react-dom";
 
 function App() {
     const [state, setState] = useState("CLICKss ME");
 
-    return <button onClick={() => setState("CLICKED")}>{state}</button>;
+    return <button className='p-5 bg-red-500' onClick={() => setState("CLICKED")}>{state}</button>;
 }
 
 render(<App />, document.getElementById("root"));
